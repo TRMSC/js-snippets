bf96f7f78bab2aaff82f3b098a702055677128f8
TRMSC <86586942+TRMSC@users.noreply.github.com>
2022-01-06 14:09:00 +0100

    Revice console notice

--- scroll-trigger.js
+++ scroll-trigger.js
@@ -1,4 +1,4 @@
-/* Scroll Trigger v.1.0 - Copyright (C) 2022, TRMSC - https://trmsc1.wordpress.com/ */
+/* Scroll Trigger v.1.0.1 - Copyright (C) 2022, TRMSC - https://trmsc1.wordpress.com/ */
 /* GNU General Public Licence 3.0 - http://www.gnu.de/documents/gpl-3.0.en.html */
 
 /* NEED TO LOAD JQUERY. IN MOODLE YOU CAN USE: */
@@ -9,6 +9,7 @@ controlTrigger("#ID OR .CLASS"); // COPY AND ADJUST THIS LINE FOR MORE SCROLL TR
 /* --- */
 
 function controlTrigger(trg) {
+    console.log('Start scroll-trigger for ' + trg);
     jQuery(trg).css("transition", "all 0.7s ease-in 0s");
     jQuery(window).scroll(function() {
 
@@ -17,10 +18,8 @@ function controlTrigger(trg) {
            wH = jQuery(window).height(),
            wS = jQuery(this).scrollTop();
        if ((wS > (hT-400)) && (wS < (hT+hH-180))) {
-           console.log('#IDNAME is in the scroll area!');
            jQuery(trg).css("opacity", "1");
        } else {
-           console.log('#IDNAME is outsideis of the scroll area!');
            jQuery(trg).css("opacity", "0");
        }
     });
