b53e57b4c9f9a77133529ef09b015af8b621066f
TRMSC <86586942+TRMSC@users.noreply.github.com>
2022-01-08 13:51:47 +0100

    Update scroll-trigger.js

--- scroll-trigger.js
+++ scroll-trigger.js
@@ -1,18 +1,31 @@
-/* Scroll Trigger v.1.0.2 - Copyright (C) 2022, TRMSC - https://trmsc1.wordpress.com/ */
+/* Scroll Trigger v.1.1 - Copyright (C) 2022, TRMSC - https://trmsc1.wordpress.com/ */
 /* GNU General Public Licence 3.0 - http://www.gnu.de/documents/gpl-3.0.en.html */
 
 /* NEED TO LOAD JQUERY. IN MOODLE YOU CAN USE: */
 /* https://github.com/TRMSC/moodle-editor-inline-codes/blob/main/jquery%20in%20moodle.html */
 
-controlTrigger("#ID OR .CLASS"); // COPY AND ADJUST THIS LINE FOR MORE SCROLL TRIGGERED ELEMENTS
+triggerIdCount = 0;
+scrollTrigger ("#id .class or element", "right or left"); // ADJUST THIS LINE. COPY THIS LINE FOR MORE SCROLL TRIGGERED ELEMENTS
 
-/* --- */
-
-function controlTrigger(trg) {
-    console.log('Start scroll-trigger for ' + trg);
+function scrollTrigger (trg, trgDir) {
+    if(trg.includes("#")) {
+        console.log("Scroll Trigger found: " + trg);
+        triggerFromLeft(trg);
+    } else {
+        jQuery(trg).each(function () {
+            if(this.id == 0) {
+                jQuery(this).attr("id", "triggerID" + triggerIdCount);
+                console.log("Scroll Trigger added: #triggerID" + triggerIdCount);
+                triggerIdCount ++;
+            } else {
+                console.log("Scroll Trigger found: #" + this.id);
+            }
+            triggerFromLeft("#" + this.id);
+        });
+    }
+    console.log('Start scroll-trigger from left for ' + trg + " from " + trgDir);
     jQuery(trg).css("transition", "all 0.7s ease-in 0s");
     jQuery(window).scroll(function() {
-
        var hT = jQuery(trg).offset().top,
            hH = jQuery(trg).outerHeight(),
            wH = jQuery(window).height(),
@@ -22,7 +35,12 @@ function controlTrigger(trg) {
            jQuery(trg).css("transform", "translate(0px, 0px)");
        } else {
            jQuery(trg).css("opacity", "0");
-           jQuery(trg).css("transform", "translate(-50px, 0px)");
+           if (trgDir == "right") {
+               jQuery(trg).css("transform", "translate(50px, 0px)");
+           } else {
+               jQuery(trg).css("transform", "translate(-50px, 0px)");
+           }
        }
     });
+    return;
 }
